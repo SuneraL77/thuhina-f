724350d1ffd8e05c8706fb58808cab07c51e7837
Malindu <ma20lindu@gmail.com>
2024-04-02 18:16:51 +0530

    create pending user and active user admin side

--- src/pages/admin/user/UserManagement.jsx
+++ src/pages/admin/user/UserManagement.jsx
@@ -1,10 +1,10 @@
 import React from 'react'
-import { Button, Input, Switch, Table  } from '@mantine/core';
+import { Button, Input, Switch, Table, Tabs } from '@mantine/core';
 import { IoSearchSharp } from "react-icons/io5";
 import { HiOutlineDotsHorizontal } from "react-icons/hi";
 import { GoDotFill } from "react-icons/go";
- 
-import { Popover   } from '@mantine/core';
+
+import { Popover } from '@mantine/core';
 
 
 const elements = [
@@ -21,9 +21,32 @@ const elements = [
 
 const UserManagement = () => {
 
+    const PendingUsersRows = elements.map((element) => (
+        <Table.Tr key={element.id}>
+            <Table.Td>{element.id}</Table.Td>
+            <Table.Td>{element.username}</Table.Td>
+            <Table.Td>{element.joineddate}</Table.Td>
+            <Table.Td>{element.ad_running}</Table.Td>
  
+            <Table.Td>{element.package}</Table.Td>
+            <Popover trapFocus position="bottom" withArrow shadow="md">
+                <Popover.Target>
+                    <Table.Td>{element.action}</Table.Td>
+                </Popover.Target>
+                <Popover.Dropdown>
+                    <div className='flex flex-col gap-3'>
+                        <h1 className='cursor-pointer hover:text-[#D5BD81]'>View User Profile</h1>
+                        <div className='flex items-center gap-2'>
+                            <span className='cursor-pointer hover:text-[#D5BD81]'>Verify User</span>
+                            <Switch />
+                        </div>
+                    </div>
+                </Popover.Dropdown>
+            </Popover>
+        </Table.Tr>
+    ));
 
-    const rows = elements.map((element) => (
+    const ActiveUsersRows = elements.map((element) => (
         <Table.Tr key={element.id}>
             <Table.Td>{element.id}</Table.Td>
             <Table.Td>{element.username}</Table.Td>
@@ -31,7 +54,7 @@ const UserManagement = () => {
             <Table.Td>{element.ad_running}</Table.Td>
             <Table.Td>{element.status}</Table.Td>
             <Table.Td>{element.package}</Table.Td>
-            <Popover   trapFocus position="bottom" withArrow shadow="md">
+            <Popover trapFocus position="bottom" withArrow shadow="md">
                 <Popover.Target>
                     <Table.Td>{element.action}</Table.Td>
                 </Popover.Target>
@@ -39,7 +62,7 @@ const UserManagement = () => {
                     <div className='flex flex-col gap-3'>
                         <div className='flex items-center gap-2'>
                             <span className='cursor-pointer hover:text-[#D5BD81]'>Block User</span>
-                            <Switch/>
+                            <Switch />
                         </div>
                         <h1 className='cursor-pointer hover:text-[#D5BD81]'>View User Profile</h1>
                         <h1 className='cursor-pointer hover:text-[#D5BD81]'>Delete User</h1>
@@ -49,31 +72,80 @@ const UserManagement = () => {
         </Table.Tr>
     ));
 
+ 
+    
     return (
         <div className='flex flex-col gap-10'>
             <h1 className='text-xl font-medium'>User Management</h1>
 
-            <div className='flex items-center justify-between w-full'>
-                <Input placeholder="Search users from ID" rightSection={<IoSearchSharp />} />
-                <Button variant='filled' color='#D5BD81'>Register New user</Button>
-            </div>
+            <Tabs defaultValue="Pending" variant="pills" color='#D5BD81'>
+                <Tabs.List>
+                    <Tabs.Tab value="Pending" >
+                        Pending Users
+                    </Tabs.Tab>
+                    <Tabs.Tab value="Active" >
+                        Active Users
+                    </Tabs.Tab>
 
-            <div className='overflow-x-scroll bg-white scroll-smooth scrollbar-hide'>
-                <Table striped  >
-                    <Table.Thead >
-                        <Table.Tr>
-                            <Table.Th>User Id</Table.Th>
-                            <Table.Th>User Name</Table.Th>
-                            <Table.Th>Joined Date</Table.Th>
-                            <Table.Th>Ad Running</Table.Th>
-                            <Table.Th>Status</Table.Th>
-                            <Table.Th>Package</Table.Th>
-                            <Table.Th>Action</Table.Th>
-                        </Table.Tr>
-                    </Table.Thead>
-                    <Table.Tbody>{rows}</Table.Tbody>
-                </Table>
-            </div>
+
+                </Tabs.List>
+
+                <Tabs.Panel value="Pending">
+                    <div className='flex flex-col w-full gap-10 py-10'>
+                        <div className='flex items-center justify-between w-full'>
+                            <Input placeholder="Search users from ID" rightSection={<IoSearchSharp />} />
+                            <Button variant='filled' color='#D5BD81'>Register New user</Button>
+                        </div>
+
+                        <div className='overflow-x-scroll bg-white scroll-smooth scrollbar-hide'>
+                            <Table striped  >
+                                <Table.Thead >
+                                    <Table.Tr>
+                                        <Table.Th>User Id</Table.Th>
+                                        <Table.Th>User Name</Table.Th>
+                                        <Table.Th>Joined Date</Table.Th>
+                                        <Table.Th>Ad Running</Table.Th>
+                             
+                                        <Table.Th>Package</Table.Th>
+                                        <Table.Th>Action</Table.Th>
+                                    </Table.Tr>
+                                </Table.Thead>
+                                <Table.Tbody>{PendingUsersRows}</Table.Tbody>
+                            </Table>
+                        </div>
+                    </div>
+                </Tabs.Panel>
+
+                <Tabs.Panel value="Active">
+                <div className='flex flex-col w-full gap-10 py-10'>
+                        <div className='flex items-center justify-end w-full'>
+                            <Input placeholder="Search users from ID" rightSection={<IoSearchSharp />} />
+                       
+                        </div>
+
+                        <div className='overflow-x-scroll bg-white scroll-smooth scrollbar-hide'>
+                            <Table striped  >
+                                <Table.Thead >
+                                    <Table.Tr>
+                                        <Table.Th>User Id</Table.Th>
+                                        <Table.Th>User Name</Table.Th>
+                                        <Table.Th>Joined Date</Table.Th>
+                                        <Table.Th>Ad Running</Table.Th>
+                                        <Table.Th>Status</Table.Th>
+                                        <Table.Th>Package</Table.Th>
+                                        <Table.Th>Action</Table.Th>
+                                    </Table.Tr>
+                                </Table.Thead>
+                                <Table.Tbody>{ActiveUsersRows}</Table.Tbody>
+                            </Table>
+                        </div>
+                    </div>
+                </Tabs.Panel>
+
+
+
+            </Tabs>
+
         </div>
     )
 }
